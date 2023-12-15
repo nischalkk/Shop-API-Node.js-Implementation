61bc965d1ee3b36f41fb4a334df8336912e09efa
Nischal G <srinivasnischal@gmail.com>
2023-12-15 18:37:32 +0530

    implement orders route

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,14 +3,27 @@ const router =  express.Router()
 const mongoose = require('mongoose')
 
 const Order = require('../models/order')
+const Product = require('../models/products')
+
+
 
 router.get('/',(req,res,next)=>{
     Order.find()
     .select('_id product quantity')
-    .then(orders=>{
-        console.log(orders);
+    .then(docs=>{
         res.status(200).json({
-            orders
+            count:docs.length,
+            orders:docs.map(doc=>{
+                return{
+                    _id: doc._id,
+                    product: doc.product,
+                    quantity: doc.quantity,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3002/orders/'+doc._id
+                    }
+                }
+            })
         })
     })
     .catch(err=>{
@@ -28,7 +41,18 @@ router.post('/',(req,res,next)=>{
     order.save()
     .then(result=>{
         console.log(result);
-        res.status(201).json(result);
+        res.status(201).json({
+            message: 'Order stored',
+            createdOrder: {
+                _id: result._id,
+                    product: result.product,
+                    quantity: result.quantity
+            },
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3002/orders/'+result._id
+                    }
+        });
     })
     .catch(err=>{
         console.log(err)
